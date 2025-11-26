d35391f59ef178771e3be449d7df541aabf53dfb
Yuri <yuriw2001@gmail.com>
2025-11-26 00:07:25 -0500

    add redis connection and health test api

--- backend/nuxt.config.ts
+++ backend/nuxt.config.ts
@@ -3,6 +3,7 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true }, 
   runtimeConfig: {
-    mongoUrl: process.env.MONGO_URL
+    mongoUrl: process.env.MONGO_URL, 
+    redisUrl: process.env.REDIS_URL
   }
 })
